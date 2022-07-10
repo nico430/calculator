73aadd871b930b287e6443410655ca3ff1f6f61c
Nicolas <nico.chabanpavon@gmail.com>
2022-07-10 20:18:56 -0300

    OK: getting interactive parts of the document

---
+++ index.js
@@ -0,0 +1,5 @@
+const prevValueDisplay = document.getElementById("prev-value");
+const actualValueDisplay = document.getElementById("actual-value");
+const numberButtons = document.querySelectorAll(".numbers");
+const numberButtons = document.querySelectorAll(".operator");
+
