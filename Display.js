c2b0bee051c2046b361db9836a8e0e0c40e591b0
Nicolas Chaban <nico.chabanpavon@gmail.com>
2022-08-18 10:33:52 -0300

    OK: configure the display class to add numbers

--- Display.js
+++ Display.js
@@ -1,5 +1,13 @@
 class Display {
   constructor(prevValueDisplay,actualValueDisplay){
-    
+    this.prevValueDisplay = prevValueDisplay;
+    this.actualValueDisplay = actualValueDisplay;
+    this.calculator = new Calculator();
+    this.prevValue = "";
+    this.actualValue = "";
+  }
+
+  addNumber(number){
+    this.number = number;
   }
 }
