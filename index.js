36045527cb088d332209f5e6c2a7674200311e40
Nicolas Chaban <nico.chabanpavon@gmail.com>
2022-08-17 22:13:07 -0300

    OK: invoke calculator class and verify that works

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const prevValueDisplay = document.getElementById("prev-value");
 const actualValueDisplay = document.getElementById("actual-value");
 const numberButtons = document.querySelectorAll(".numbers");
-const numberButtons = document.querySelectorAll(".operator");
+const operatorButtons = document.querySelectorAll(".operator");
 
+const calculator = new Calculator();
+
+console.log(calculator.add(2,2));
