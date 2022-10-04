907de8978aab627000c936080b4757e3c471424b
Nicolas Chaban <nicolas@enve.io>
2022-10-04 00:13:56 -0300

    OK: adding delete method to display

--- Display.js
+++ Display.js
@@ -7,6 +7,10 @@ class Display {
     this.actualValue = "";
   }
 
+  delete(){
+    this.actualValue = this.actualValue.toString().slice(0,-1)
+  }
+
   addNumber(number){
     if(number==='.' && this.actualValue.includes('.')) return
     this.actualValue = this.actualValue.toString() + number.toString();
